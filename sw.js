a62b563313b8fcb422ee975a90e72f013ba60e6c
AlTeveDev <73194036+AlTeveDev@users.noreply.github.com>
2024-05-02 14:52:41 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,26 @@
+const CACHE_NAME = 'powerpoint-pwa_example';
+
+const INITIAL_CACHED_FILES = [
+  '/pwa_example'
+]
+
 self.addEventListener('install', function(e) {
-  console.log('install!')
-})
+  console.log('install!');
+  e.waitUntill(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      cache.addAll(INITIAL_CACHED_FILES);
+    })(),
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  const cache = await.caches.open(CACHE_NAME);
+
+  const cachedResponse = await cache.match(event.request);
+  if (cachedResponse !== undefined) {
+    return cachedResponse;
+  } else {
+    return fetch(e.request.url);
+  }
+});
